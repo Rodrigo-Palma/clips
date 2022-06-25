293099d79b13e00667066bf6a5826cec6be3522e
Rodrigo-Palma <email.rodrigopalma@gmail.com>
2022-06-25 16:14:34 -0300

    improves in auth mode

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 import IUser from '../models/user.model';
+import { map } from 'rxjs/operators';
+
 
 
 @Injectable({
@@ -9,12 +12,16 @@ import IUser from '../models/user.model';
 })
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>
+  public isAuthenticated$: Observable<boolean>
 
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore
   ) {
-    this.usersCollection = db.collection('user');
+    this.usersCollection = db.collection('user')
+    this.isAuthenticated$ = auth.user.pipe(
+      map(user => !!user)
+    )
    }
 
   public async createUser(userData: IUser) {
