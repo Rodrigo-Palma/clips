cd313a2e5bf8bd15cc3a0ba84de3facb3a86ee9a
Rodrigo-Palma <email.rodrigopalma@gmail.com>
2022-06-25 10:54:47 -0300

    adding firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCjhidffJYzrb7VsymCrJV7BE317b6kl5o",
+    authDomain: "clips-d2bbb.firebaseapp.com",
+    projectId: "clips-d2bbb",
+    storageBucket: "clips-d2bbb.appspot.com",
+    appId: "1:592068177540:web:45d4496594f26ce6881cd1"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCjhidffJYzrb7VsymCrJV7BE317b6kl5o",
+    authDomain: "clips-d2bbb.firebaseapp.com",
+    projectId: "clips-d2bbb",
+    storageBucket: "clips-d2bbb.appspot.com",
+    appId: "1:592068177540:web:45d4496594f26ce6881cd1"
+  }
 };
 
 /*
