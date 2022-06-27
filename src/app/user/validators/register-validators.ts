76ecd31c5a18b007815cb26db57f623ad566029d
Rodrigo-Palma <email.rodrigopalma@gmail.com>
2022-06-26 23:40:32 -0300

    make improvement in password validator

--- src/app/user/validators/register-validators.ts
+++ src/app/user/validators/register-validators.ts
@@ -1,18 +1,22 @@
-import { ValidationErrors, AbstractControl } from "@angular/forms"
+import { ValidationErrors, AbstractControl, ValidatorFn } from "@angular/forms"
 
 export class RegisterValidators {
-  static match(controlName: string, matchingControlName: string) {
+  static match(controlName: string, matchingControlName: string) : ValidatorFn {
     return (group: AbstractControl)  : ValidationErrors | null=> {
-      const control = group.get('password')
+      const control = group.get(controlName)
       const matchingControl = group.get(matchingControlName)
 
       if(!control || !matchingControl) {
+        console.log('Form controls can not be found in the form group.')
         return { controlNotFound: false }
       }
 
       const error = control.value === matchingControl.value ?
       null :
       { noMatch: true }
+
+      matchingControl.setErrors(error)
+
       return error
 
     }
