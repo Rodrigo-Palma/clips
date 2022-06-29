b5c81e1dd394fb27d3815d013c7281ff6e062874
Rodrigo-Palma <email.rodrigopalma@gmail.com>
2022-06-28 21:40:05 -0300

    forcing redirection

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,20 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
+
 export class NavComponent implements OnInit {
  constructor(
     public modal: ModalService,
     public auth: AuthService,
-    private afAuth: AngularFireAuth
+    private afAuth: AngularFireAuth,
+    private router: Router
   ) {
-
-  }
+}
 
 
   ngOnInit(): void {
@@ -32,5 +34,7 @@ export class NavComponent implements OnInit {
     $event.preventDefault()
 
     await this.afAuth.signOut()
+
+    await this.router.navigateByUrl('/')
   }
 }
