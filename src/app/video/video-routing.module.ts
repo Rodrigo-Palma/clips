741e66873c18dabd2c3a4dc4a88cb71878ffe5cc
Rodrigo-Palma <email.rodrigopalma@gmail.com>
2022-06-29 09:21:23 -0300

    added component not-found(404) and some improvements in routes

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -2,20 +2,33 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
 import { UploadComponent } from './upload/upload.component';
+import { AuthGuardModule, redirectUnauthorizedTo } from '@angular/fire/auth-guard'; //
+
+
+const redirectUnauthorizedToHome = () => redirectUnauthorizedTo('/')
+
 
 const routes: Routes = [
   {
     path: 'manage',
     component: ManageComponent,
     data: {
-      authOnly: true
-    }
+      authOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome
+    },
+    canActivate: [AuthGuardModule]
   }, {
     path: 'upload',
     component: UploadComponent,
     data: {
-      authOnly: true
-    }
+      authOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome
+    },
+    canActivate: [AuthGuardModule]
+  },
+  {
+    path: 'manage-clips',
+    redirectTo: 'manage'
   }
 ];
 
