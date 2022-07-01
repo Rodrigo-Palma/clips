c12590d4811947a7e093257cbe11b7510fcaf9d2
Rodrigo-Palma <email.rodrigopalma@gmail.com>
2022-06-30 21:55:15 -0300

    created edit component to edit videos in manage

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ClipService } from 'src/app/services/clip.service';
 import IClip from 'src/app/models/clip.model';
+import { ModalService } from 'src/app/services/modal.service';
 
 
 @Component({
@@ -16,7 +17,8 @@ export class ManageComponent implements OnInit {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private clipService: ClipService
+    private clipService: ClipService,
+    private modal: ModalService
   ) { }
 
 
@@ -46,5 +48,9 @@ export class ManageComponent implements OnInit {
         sort: value
       }
     })
+  }
+  openModal($event: Event, clip: IClip ) {
+    $event.preventDefault()
+    this.modal.toggleModal('editClip')
   }
 }
