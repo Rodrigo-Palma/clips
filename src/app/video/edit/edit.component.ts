507738bb4deef47eda504487f2625cbab8fa9030
Rodrigo-Palma <email.rodrigopalma@gmail.com>
2022-07-01 11:05:27 -0300

    updating clips

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, OnChanges, Output, EventEmitter } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 import IClip from 'src/app/models/clip.model';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
@@ -14,6 +14,7 @@ export class EditComponent implements OnInit, OnDestroy, OnChanges {
   showAlert = false
   alertColor = 'blue'
   alertMsg = 'Please wait! Updating clip.'
+  @Output() update = new EventEmitter()
 
   clipID = new FormControl('', {
     nonNullable: true
@@ -57,6 +58,10 @@ export class EditComponent implements OnInit, OnDestroy, OnChanges {
   }
 
   async submit() {
+    if(!this.activeClip) {
+      return
+    }
+
     this.inSubmission = true
     this.showAlert = true
     this.alertColor = 'blue'
@@ -72,7 +77,10 @@ export class EditComponent implements OnInit, OnDestroy, OnChanges {
       this.alertColor = 'red'
       this.alertMsg = 'Something went wrong. Try again later.'
       return
-      }
+    }
+
+    this.activeClip.title = this.title.value
+    this.update.emit(this.activeClip)
 
     this.inSubmission = false
     this.alertColor = 'green'
